0607796469103645116f4eb2d0796d5baa24f68a
AshwinSomani <36534766+AshwinSomani@users.noreply.github.com>
2018-02-17 15:40:30 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/terminal-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'ashwin.html'));
+     res.sendFile("bol bhosdike");
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
