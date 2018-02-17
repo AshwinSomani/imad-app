6f9dada5c6122f450d51d2e9136934c53175fa72
AshwinSomani <36534766+AshwinSomani@users.noreply.github.com>
2018-02-17 15:04:32 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('terminal-one',function(req,res){
+app.get('/terminal-one',function(req,res){
    res.send("Prachi I Love U"); 
 });
 app.get('/ui/style.css', function (req, res) {
