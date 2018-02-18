7b8934eca0a7db4145d824b35ff27b016e3db8c2
AshwinSomani <36534766+AshwinSomani@users.noreply.github.com>
2018-02-18 07:19:29 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,73 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+
+var terminal_one={
+    title:'HELLOPAPA',
+    date:'18.12.1998',
+    heading:'terminal-one',
+    content:`
+                    <p>
+                    ..prachi i luv uprachi i luv u...prachi i luv uprachi i luv u...prachi i luv uprachi i luv uprachi i luv uprachi i luv u..prachi i luv uprachi i luv u
+                    </p>
+                    <p>
+                    ..prachi i luv uprachi i luv u...prachi i luv uprachi i luv u...prachi i luv uprachi i luv uprachi i luv uprachi i luv u..prachi i luv uprachi i luv u
+                    </p>
+                    <p>
+                    ..prachi i luv uprachi i luv u...prachi i luv uprachi i luv u...prachi i luv uprachi i luv uprachi i luv uprachi i luv u..prachi i luv uprachi i luv u
+                    </p>    
+                `
+};
+function somaniji(data){
+    var date=data.date;
+    var heading=data.heading;
+    var title=data.title;
+    var content=data.content;
+var HtmlCodes=`
+<html>
+    
+    <head>
+       <title>
+           HELLOPAPA
+        </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+              <div class="center">
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+            <div class='ashwin'>
+            <div>
+                <a href="/">ashwinsomani</a>
+                </div>
+                <hr/>
+                <h5>
+                    ${heading}
+                    </h5>
+                    <div>
+                        ${date}
+                        </div>
+                <div>
+                    ${content}
+                    </div>
+            </div>
+            </body>
+    
+</html>
+
+`;
+
+    return HtmlCodes;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/terminal-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'ashwin.html'));
+    res.sendFile(somaniji(terminal_one));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
