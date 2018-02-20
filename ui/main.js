c2ddf8b72671ad81128d1a3253712b040da098b8
AshwinSomani <36534766+AshwinSomani@users.noreply.github.com>
2018-02-20 14:38:11 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ element.innerHTML="HELLO";
 var img=document.getElementById("ash");
 img.onclick=function(){
   img.style.marginLeft="1120px";
-if(img.setOnClickListener){
-  img.style.marginRight="300px";  
+if(img.onclick){
+  img.style.marginTop="300px";  
 }
 };
