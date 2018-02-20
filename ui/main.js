e77b77bf3d6fbade9c977d11e544003707d2bd54
AshwinSomani <36534766+AshwinSomani@users.noreply.github.com>
2018-02-20 14:14:12 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("ashwin");
 element.innerHTML="HELLO";
+var img=document.getElementById("ash");
+img.onclick=function(){
+  img.style.marginRight="100px";  
+};
