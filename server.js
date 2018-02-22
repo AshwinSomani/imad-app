3bd287dcf982a437800377393639f3937a790333
AshwinSomani <36534766+AshwinSomani@users.noreply.github.com>
2018-02-22 03:22:36 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ashwin',function(req,res){
+app.get('ashwin',function(req,res){
   
    res.send("hello");
 });
