d6855b5e696a527970e21f80dac6861774334006
AshwinSomani <36534766+AshwinSomani@users.noreply.github.com>
2018-03-03 03:51:58 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/ashwin',function(req,res){
     res.send("hello");
 });
 var pool=new Pool(config);
-app.get("/test-db",function(res,req){
+app.get("/test-db",function(req,res){
     pool.query("selext * FROM test",function(err,result){
         if(err){
             res.status(500).send(err.toString());
