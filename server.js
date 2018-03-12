aae3a65182565c231c3789d1ff8f90161b7a8b6e
AshwinSomani <36534766+AshwinSomani@users.noreply.github.com>
2018-03-12 08:26:40 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ var config={
   port:'5432',
   password:process.env.DB_PASSWORD
 };
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000, 512, 'sha512');
+    return ['pbkdf2Sync','10000', salt,hashed.toString("hex")].join('$');
+}
 app.use(bodyParser.json());
 var terminal={   
 'terminal-one':{
@@ -133,10 +137,7 @@ app.get("/test-db",function(req,res){
         }
     });
 });
-function hash(input,salt){
-    var hashed=crypto.pbkdf2Sync(input,salt,10000, 512, 'sha512');
-    return ['pbkdf2Sync','10000', salt,hashed.toString("hex")].join('$');
-}
+
 app.get('/hash/:input',function(req,res){
    var hashstring=hash(req.params.input,"this -is -some-random-string");
    res.send(hashstring);
